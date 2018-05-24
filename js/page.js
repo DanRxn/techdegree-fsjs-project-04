5f991041fccdae1382291a8e994dd00416eba3ad
Dan Robertson <dan@productintuition.com>
2018-05-24 07:36:17 -0700

    Winner selection is working. Need to get the UI to reflect the correct game state on Finish

--- js/page.js
+++ js/page.js
@@ -1,16 +1,5 @@
 (function(){
 	
-	const winningCombinations = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8],
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8],
-		[2, 4, 6],
-		[0, 4, 8]
-	];
-
 	class Player {
 		constructor(id) {
 			this.id = id;
@@ -46,10 +35,10 @@
 		
 		get winner() {
 			// Calculate winner (null if none)
-			const winner = '';
-			winningCombinations.forEach(combo, () => {
+			let winner = '';
+			winningCombinations.forEach((combo) => {
 				let comboSquares = this.board.filter(square => combo.includes(square.id));
-				let comboSquareOwners = comboSquares.map(square.owner);
+				let comboSquareOwners = comboSquares.map(square => square.owner);
 				if (comboSquareOwners.every(owner => owner === this.players.player1)) {
 					winner = this.players.player1;
 				} else if (comboSquareOwners.every(owner => owner === this.players.player2)) {
@@ -62,17 +51,6 @@
 			return winner;
 		};
 	}
-
-	let startDiv = document.querySelector('#start');
-		let startGameButton = document.querySelector('#start-game');
-	let boardDiv = document.querySelector('#board');
-		let player1Li = document.querySelector('#player1');
-		let player2Li = document.querySelector('#player2');
-		let boxes = document.querySelector('ul.boxes');
-	let finishDiv = document.querySelector('#finish');
-		let newGameButton = document.querySelector('#new-game');
-
-	let thisGame = new Game();
 
 	const startGame = () => {
 		thisGame = new Game();
@@ -110,23 +88,6 @@
 		});
 	}
 
-	const claimSquare = (squareId) => {
-		// Check if square is unclaimed
-		if (thisGame.board[squareId].owner === '') {
-			// Update square.owner
-			thisGame.board[squareId].owner = thisGame.activePlayer;
-			// Check for winner
-			if (thisGame.winner != '') {
-				thisGame.isEnded = true;
-			} else if (thisGame.activePlayer.id === '1') { // If no winner, update active player and redraw squares
-				thisGame.activePlayer = thisGame.players.player2;
-			} else {
-				thisGame.activePlayer = thisGame.players.player1;
-			}
-			drawPage();
-		} 
-	}
-
 	const writeStartHtml = () => {
 		startDiv.style.display = '';
 		boardDiv.style.display = 'none';
@@ -141,6 +102,12 @@
 		highlightActivePlayer();
 		drawBoxes();
 	}
+
+	const writeFinishHtml = () => {
+		startDiv.style.display = 'none';
+		boardDiv.style.display = 'none';
+		finishDiv.style.display = '';
+	}
 	
 	const drawPage = () => {
 		if (!thisGame.isStarted) {
@@ -152,11 +119,50 @@
 		}
 	}
 
-	const writeFinishHtml = () => {
-		startDiv.style.display = 'none';
-		boardDiv.style.display = 'none';
-		finishDiv.style.display = '';
+	const getClickedElementIndex = (element) => {
+		return [...element.parentNode.children].indexOf(element);
 	}
+
+	const updateClickedSquare = (squareId) => {
+		// Check if square is unclaimed
+		if (thisGame.board[squareId].owner === '') {
+			// Update square.owner
+			thisGame.board[squareId].owner = thisGame.activePlayer;
+			// Check for winner
+			if (thisGame.winner != '') {
+				debugger;
+				let winner = thisGame.winner;
+				thisGame.isEnded = true;
+			} else if (thisGame.activePlayer.id === '1') { // If no winner, update active player and redraw squares
+				thisGame.activePlayer = thisGame.players.player2;
+			} else {
+				thisGame.activePlayer = thisGame.players.player1;
+			}
+			drawPage();
+		} 
+	}
+
+	let startDiv = document.querySelector('#start');
+		let startGameButton = document.querySelector('#start-game');
+	let boardDiv = document.querySelector('#board');
+		let player1Li = document.querySelector('#player1');
+		let player2Li = document.querySelector('#player2');
+		let boxes = document.querySelector('ul.boxes');
+	let finishDiv = document.querySelector('#finish');
+		let newGameButton = document.querySelector('#new-game');
+
+	const winningCombinations = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[2, 4, 6],
+		[0, 4, 8]
+	];
+
+	let thisGame = new Game();
 
 	drawPage(thisGame);
 
@@ -166,13 +172,8 @@
 	});
 
 	document.querySelector('.boxes').addEventListener('click', (e) => {
-		// Figure out which box ID got clicked 
 		const clickedSquare = e.target; 
-		const getElementIndex = (element) => {
-			return [...element.parentNode.children].indexOf(element);
-		}
-		const squareId = getElementIndex(clickedSquare);
-		claimSquare(squareId);
+		updateClickedSquare(getClickedElementIndex(clickedSquare));
 	});
 
 })();
